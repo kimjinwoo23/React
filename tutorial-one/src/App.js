16ca8f80d6933cad873bc68401b6d5d121ed380e
kimjinwoo23 <heastoria@naver.com>
2024-07-03 11:36:15 +0900

    1234

---
+++ tutorial-one/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+/*
+html 에서 css사용할땐 class 지만 
+react 에선 className으로 사용 
+
+css js 를 가져올때 import를 사용 
+
+css 가져올땐 
+import css경로와 파일명 
+
+js 를 가져올때는 
+import [여기서 사용할 이름 ] from 'javascript파일경로 / javascript 파일명';
+
+R01_classComponent.js 를 가져와서 app.js에 적용 
+R01_classComponent.js R01이라는 별칭을 사용해서 app.js에 적용
+
+*/
+function App() {
+  return (
+    <div className="App-header">
+
+    </div>
+  );
+}
+
+export default App;
